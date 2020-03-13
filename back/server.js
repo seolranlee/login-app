747b73c48130e043f7a083632cc5b94f23670df9
seolranlee <leesugar90@gmail.com>
2020-03-13 10:33:51 +0900

    [login-app] - sign up 기능 구현 - Login 컴포넌트 클래스형으로 변환

--- back/server.js
+++ back/server.js
@@ -10,10 +10,16 @@ const utils = require('./utils');
 const app =  express();
 const port = process.env.PORT || 6899;
 
-const userData = {
-    email: "leesugar90@naver.com",
-    password: "1234",
-};
+const users = [
+    {
+        email: "leesugar90@naver.com",
+        password: "1234",
+    },
+    {
+        email: "leesugar90@google.com",
+        password: "1234",
+    }
+];
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -52,13 +58,50 @@ app.post('/users/signin', function(req, res){
             message: 'email or password required.'
         })
     }
-
-    if( email !== userData.email || password !== userData.password ){
+    
+    if( users.filter(function (user) { return user.email == email }).length === 0 || password !== users.filter(function (user) { return user.email == email })[0].password ){
         return res.status(401).json({
             error: true,
             message: 'email or password is wrong'
         })
     }
+
+    const userData = {
+        email,
+        password
+    }
+
+    const token = utils.generateToken(userData);
+    const userObj = utils.getCleanUser(userData);
+
+    return res.json({ user: userObj, token });
+
+});
+
+app.post('/users/signup', function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if(!email || !password) {
+        return res.status(400).json({
+            error: true,
+            message: 'email or password required.'
+        })
+    }
+    
+    if( users.filter(function (user) { return user.email == email }).length !== 0 ){
+        return res.status(401).json({
+            error: true,
+            message: '이미 가입된 email입니다.'
+        })
+    }
+
+    const userData = {
+        email,
+        password
+    }
+
+    users.push(userData);
 
     const token = utils.generateToken(userData);
     const userObj = utils.getCleanUser(userData);
--- front/src/Login.js
+++ front/src/Login.js
@@ -1,56 +1,116 @@
-import React, { useState } from 'react';
+// start
+
+import React, { useState, Component } from 'react';
 import axios from 'axios'
 import {setUserSession} from './Utils/Common'
 
+class Login extends Component {
 
-const Login = (props) => {
-    const email = useFormInput('');
-    const password = useFormInput('');
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+    state = {
+        isSignUp: false,
+        error: null,
+        loading: false,
+        email: '',
+        password: '',
+        confirm_password: '',
+        users: [
+            {
+                email: "leesugar90@naver.com",
+                password: "1234",
+            },
+            {
+                email: "leesugar90@google.com",
+                password: "1234",
+            }
+        ],
 
-    // handle button click of login form
-    const handleLogin = () => {
-        setError(null);
-        setLoading(true);
-        axios.post('http://110.10.189.209:6899/users/signin', { email: email.value, password: password.value}).then(response=>{
-            setLoading(false);
+    }
+    
+    handleLogin = () => {
+        axios.post('http://110.10.189.209:6899/users/signin', { email: this.state.email, password: this.state.password} ).then(response=>{
+            this.setState({
+                loading: false,
+            })
             setUserSession(response.data.token, response.data.user);
-            props.history.push('/dashboard');
+            this.props.history.push('/dashboard');
         }).catch(error=>{
-            setLoading(false);
-            if(error.response.status === 401 || error.response.status === 400) setError(error.response.data.message);
-            else setError('알수없는 문제로 인한 에러')
+            this.setState({
+                loading: false,
+            })
+            if(error.response.status === 401 || error.response.status === 400) this.setState({ error: error.response.data.message});
+            else this.setState({ error: '알수 없는 에러'})
         });
         // props.history.push('/dashboard')
     }
 
-    return (
-        <div>
-            Login<br /><br />
-            <div>
-                Email<br />
-                <input type="text" {...email} />
-            </div>
-            <div style={{marginTop: 10}}>
-                Password<br />
-                <input type="password" {...password} />
-            </div>
-            {error && <div><samll style={{color:'red'}}>{error}</samll></div>}<br/>
-            <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disable={loading} /><br />
-        </div>
-    );
-};
-
-const useFormInput = initialValue => {
-    const [value, setValue] = useState(initialValue);
-
-    const handleChange = e =>{
-        setValue(e.target.value);
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
-    return {
-        value, 
-        onChange: handleChange
+
+    toggleSignUp = () => {
+        this.setState({
+            isSignUp: !this.state.isSignUp,
+            email: '',
+            password: '',
+            error: null,
+        })
+    }
+
+    handleSignUp = () => {
+        const {email, password, confirm_password} = this.state;
+        console.log(email, password, confirm_password)
+        if(password!==confirm_password){
+            this.setState({ error: '확인된 비밀번호가 틀립니다.'})
+        }else{
+            axios.post('http://110.10.189.209:6899/users/signup', { email: this.state.email, password: this.state.password} ).then(response=>{
+                this.setState({
+                    loading: false,
+                })
+                setUserSession(response.data.token, response.data.user);
+                this.props.history.push('/dashboard');
+            }).catch(error=>{
+                this.setState({
+                    loading: false,
+                })
+                if(error.response.status === 401 || error.response.status === 400) this.setState({ error: error.response.data.message});
+                else this.setState({ error: '알수 없는 에러'})
+            });
+        }
+        
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                Login<br /><br />
+                <div>
+                    Email<br />
+                    <input type="text" name="email" value={this.state.email} onChange={this.handleChange}/>
+                </div>
+                <div style={{marginTop: 10}}>
+                    Password<br />
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                </div>
+                {
+                    this.state.isSignUp &&
+                    <div style={{marginTop: 10}}>
+                        Confirm Password<br />
+                        <input type="password" name="confirm_password" value={this.state.confirm_password} onChange={this.handleChange}/>
+                        <div><small style={{color:'red'}}>{this.state.error}</small></div>
+                    </div>
+                }
+                {/* <div><small style={{color:'red'}}>{this.state.error}</small></div><br /> */}
+                {this.state.error&&!this.state.isSignUp && <div><small style={{color:'red'}}>{this.state.error}</small></div>}<br/>
+                {!this.state.isSignUp && <input type="button" value={this.state.loading ? 'Loading...' : 'Login'} onClick={this.handleLogin}/>}<br/>
+                {this.state.isSignUp && <input type="button" value="submit" onClick={this.handleSignUp}/>}<br/>
+                <input type="button" value={!this.state.isSignUp? 'Sign up':'Login'} onClick={this.toggleSignUp}/><br />
+            </div>
+        );
     }
 }
+
 export default Login;
